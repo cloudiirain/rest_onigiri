0a8debab60118f19bb35b71b756e644b86e170a8
cloudiirain <muffinzapplepear@gmail.com>
2015-07-29 22:01:01 +0000

    Fixed some small model bugs

--- directory/models.py
+++ directory/models.py
@@ -5,45 +5,43 @@ class Series(ModeratedModel):
     """
     Series: Model representing a collection of volumes or books
     """
-    title = models.CharField(max_length=100, default="")
+    name = models.CharField(max_length=100, default="")
     synopsis = models.TextField(blank=True, default="")
 
     def __unicode__(self):
-        return self.title
+        return self.name
+
+class Volume(ModeratedModel):
+    name = models.CharField(max_length=100, default="")
+    series = models.ForeignKey(Series, null=True)
+    order = models.IntegerField(null=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return str(self.series) + ": " + self.name
 
     class Meta:
-        ordering = ['title',]
+        unique_together = ('series', 'order')
+        ordering = ['series', 'order']
+
+class Chapter(ModeratedModel):
+    name = models.CharField(max_length=200, default="")
+    volume = models.ForeignKey(Volume, null=True)
+    translator = models.CharField(max_length=50, default="", blank=True)
+    url = models.URLField(null=True)
+    order = models.IntegerField(null=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return str(self.volume) + " " + self.name
+
+    class Meta:
+        ordering = ['volume', 'order']
         
 """
 class RecentActivity(models.Model):
     # Use this class to track who changed each item of content and when
     # On save, track all relevant information
 
-class Volume(models.Model):
-   
-    title = models.CharField(max_length=100, default="")
-    series = models.ForeignKey(Series, null=True)
-    order = models.IntegerField(null=True)
-    created = models.DateTimeField(auto_now_add=True)
 
-    def __unicode__(self):
-        return str(self.series) + ": " + self.title
-
-    class Meta:
-        unique_together = ('series', 'order')
-        ordering = ['series', 'order']
-
-class Chapter(models.Model):
-    title = models.CharField(max_length=200, default="")
-    volume = models.ForeignKey(Volume, null=True)
-    translator = models.CharField(max_length=50, default="")
-    url = models.URLField(null=True)
-    order = models.IntegerField(null=True)
-    created = models.DateTimeField(auto_now_add=True)
-
-    def __unicode__(self):
-        return str(self.volume) + " " + self.title
-
-    class Meta:
-        ordering = ['volume', 'order']
 """
--- directory/tests.py
+++ directory/tests.py
@@ -1,36 +1,46 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from directory.models import Series
+from directory.models import Series, Volume, Chapter
 
 class ModerationTestCase(TestCase):
     def setUp(self):
-        Series.objects.create(title="Oreshura", synopsis="Masuzu does cool things.")
+        Series.objects.create(name="Oreshura", synopsis="Masuzu does cool things.")
         User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
 
     def test_unmoderated_object(self):
-        series = Series.unmoderated_objects.get(title="Oreshura")
+        series = Series.unmoderated_objects.get(name="Oreshura")
         self.assertEqual(series.moderated_object.moderation_status, 2)
         self.assertEqual(len(Series.objects.all()), 0)
         self.assertEqual(len(Series.unmoderated_objects.all()), 1)
         self.assertEqual(series.synopsis, "Masuzu does cool things.")
     
     def test_moderated_object(self):
-        series = Series.unmoderated_objects.get(title="Oreshura")
+        series = Series.unmoderated_objects.get(name="Oreshura")
         user = User.objects.get(username="john")
         series.moderated_object.approve(moderated_by=user, reason='Test approval')
         self.assertEqual(series.moderated_object.moderation_status, 1)
-        approved = Series.objects.get(title="Oreshura")
+        approved = Series.objects.get(name="Oreshura")
         self.assertEqual(approved.synopsis, "Masuzu does cool things.")
 
-class SeriesTestCase(TestCase):
+class SeriesVolumeChapterTestCase(TestCase):
     def setUp(self):
-        Series.objects.create(title="Oreshura", synopsis="Masuzu does cool things.")
-    
-    """
-    def test_unicode(self):
-        oreshura = Series.objects.get(title="Oreshura")
-        sao = Series.objects.get(title="Sword Art Online")
+        series = Series.objects.create(name="Oreshura", synopsis="Masuzu does cool things.")
+        volume = Volume.objects.create(name="Volume 1", series=series, order=1)
+        chapter = Chapter.objects.create(name="Chapter 1", volume=volume, order=1)
+        user = user = User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+        
+        series.moderated_object.approve(moderated_by=user, reason="test")
+        volume.moderated_object.approve(moderated_by=user, reason="test")
+        chapter.moderated_object.approve(moderated_by=user, reason="test")
+
+    def test_series_unicode(self):
+        oreshura = Series.objects.get(name="Oreshura")
         self.assertEqual(oreshura.__unicode__(), u'Oreshura')
-        self.assertEqual(sao.synopsis, u'Kirito sux.')
-    """
-    
+        
+    def test_volume_unicode(self):
+        oreshura = Volume.objects.get(pk=1)
+        self.assertEqual(oreshura.__unicode__(), u'Oreshura: Volume 1')
+        
+    def test_chapter_unicode(self):
+        oreshura = Chapter.objects.get(pk=1)
+        self.assertEqual(oreshura.__unicode__(), u'Oreshura: Volume 1 Chapter 1')
