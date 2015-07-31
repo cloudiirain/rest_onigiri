b1ad22816ef3e426b1c79e16feb0161e1c4bbc26
cloudiirain <muffinzapplepear@gmail.com>
2015-07-31 03:12:57 -0400

    Browsable api and updated views to use rest views

--- directory/urls.py
+++ directory/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import include, url
+from rest_framework.urlpatterns import format_suffix_patterns
 from directory import views
 
 urlpatterns = [
     url(r'^series/$', views.series_list),
     url(r'^series/(?P<pk>[0-9]+)/$', views.series_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- directory/views.py
+++ directory/views.py
@@ -1,58 +1,47 @@
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from directory.models import Tag, Creator, Contributor, Series, SeriesAlias, SeriesRating, Volume, Chapter, Translation
 from directory.serializers import *
 
-class JSONResponse(HttpResponse):
-    '''
-    An HttpResponse that renders its content into JSON.
-    '''
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
-
-@csrf_exempt
-def series_list(request):
+@api_view(['GET', 'POST'])
+def series_list(request, format=None):
     '''
     List all series, or create a new series
     '''
     if request.method == 'GET':
         series = Series.objects.all()
         serializer = SeriesSerializer(series, many=True)
-        return JSONResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = SeriesSerializer(data=data)
+        serializer = SeriesSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JSONResponse(serializer.data, status=201)
-        return JSONResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
-def series_detail(request, pk):
+@api_view(['GET', 'PUT', 'DELETE'])
+def series_detail(request, pk, format=None):
     """
     Retrieve, update or delete a series.
     """
     try:
         series = Series.objects.get(pk=pk)
     except Series.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = SeriesSerializer(series)
-        return JSONResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = SeriesSerializer(series, data=data)
+        serializer = SeriesSerializer(series, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JSONResponse(serializer.data)
-        return JSONResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         series.delete()
