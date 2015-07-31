52004cc5b20f1825cba1610d02efef9370db9ab2
cloudiirain <muffinzapplepear@gmail.com>
2015-07-31 03:26:56 -0400

    Class-based views tested

--- directory/urls.py
+++ directory/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from directory import views
 
 urlpatterns = [
-    url(r'^series/$', views.series_list),
-    url(r'^series/(?P<pk>[0-9]+)/$', views.series_detail),
+    url(r'^series/$', views.SeriesList.as_view()),
+    url(r'^series/(?P<pk>[0-9]+)/$', views.SeriesDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- directory/views.py
+++ directory/views.py
@@ -1,10 +1,62 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
+from rest_framework import generics
 from directory.models import Tag, Creator, Contributor, Series, SeriesAlias, SeriesRating, Volume, Chapter, Translation
-from directory.serializers import *
+from directory.serializers import SeriesSerializer
 
+class SeriesList(generics.ListCreateAPIView):
+    queryset = Series.objects.all()
+    serializer_class = SeriesSerializer
+
+class SeriesDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Series.objects.all()
+    serializer_class = SeriesSerializer
+
+"""
+class SeriesList(APIView):
+    '''
+    List all series, or create a new series.
+    '''
+    def get(self, request, format=None):
+        series = Series.objects.all()
+        serializer = SeriesSerializer(series, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = Series(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class SeriesDetail(APIView):
+    '''
+    Retrieve, update or delete a series instance.
+    '''
+    def get_object(self, pk):
+        try:
+            return Series.objects.get(pk=pk)
+        except Series.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        series = self.get_object(pk)
+        serializer = SeriesSerializer(series)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        series = self.get_object(pk)
+        serializer = SeriesSerializer(series, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        series = self.get_object(pk)
+        series.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+"""
+
+"""
 @api_view(['GET', 'POST'])
 def series_list(request, format=None):
     '''
@@ -24,9 +76,9 @@ def series_list(request, format=None):
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def series_detail(request, pk, format=None):
-    """
+    '''
     Retrieve, update or delete a series.
-    """
+    '''
     try:
         series = Series.objects.get(pk=pk)
     except Series.DoesNotExist:
@@ -46,3 +98,4 @@ def series_detail(request, pk, format=None):
     elif request.method == 'DELETE':
         series.delete()
         return HttpResponse(status=204)
+"""
